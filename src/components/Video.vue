19e82e2813fcc9bf6c8ed4efda4d10931b2d3fa8
Vinicius Fortino <vinicius.fortino@gmail.com>
2018-08-07 20:39:17 -0300

    components files added, video component created

---
+++ src/components/Video.vue
@@ -0,0 +1,12 @@
+<template>
+    <video autoplay>
+      <source src="../assets/startup.mp4" type="video/mp4">
+    </video>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Desktop extends Vue {}
+</script>
